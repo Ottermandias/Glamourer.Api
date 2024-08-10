6fb5c6d3ab7065e8491a5716ee9d53644c6a4d40
Ottermandias <nickroederspam+git1@gmail.com>
2024-08-10 11:49:36 +0200

    Add BonusItem API.

--- Helpers/FuncProvider.cs
+++ Helpers/FuncProvider.cs
@@ -161,6 +161,38 @@ public sealed class FuncProvider<T1, T2, T3, T4, TRet> : IDisposable
 }
 
 /// <inheritdoc cref="FuncProvider{TRet}"/>
+public sealed class FuncProvider<T1, T2, T3, T4, T5, TRet> : IDisposable
+{
+    private ICallGateProvider<T1, T2, T3, T4, T5, TRet>? _provider;
+
+    public FuncProvider(IDalamudPluginInterface pi, string label, Func<T1, T2, T3, T4, T5, TRet> func)
+    {
+        try
+        {
+            _provider = pi.GetIpcProvider<T1, T2, T3, T4, T5, TRet>(label);
+        }
+        catch (Exception e)
+        {
+            PluginLogHelper.WriteError(pi, $"Error registering IPC Provider for {label}\n{e}");
+            _provider = null;
+        }
+
+        _provider?.RegisterFunc(func);
+    }
+
+    public void Dispose()
+    {
+        _provider?.UnregisterFunc();
+        _provider = null;
+        GC.SuppressFinalize(this);
+    }
+
+    ~FuncProvider()
+        => Dispose();
+}
+
+
+/// <inheritdoc cref="FuncProvider{TRet}"/>
 public sealed class FuncProvider<T1, T2, T3, T4, T5, T6, TRet> : IDisposable
 {
     private ICallGateProvider<T1, T2, T3, T4, T5, T6, TRet>? _provider;
--- IpcSubscribers/Items.cs
+++ IpcSubscribers/Items.cs
@@ -13,11 +13,13 @@ public sealed class SetItem(IDalamudPluginInterface pi)
     public const string Label = $"Glamourer.{nameof(SetItem)}.V3";
 
     /// <inheritdoc cref="IGlamourerApiItems.SetItem"/>
-    public GlamourerApiEc Invoke(int objectIndex, ApiEquipSlot slot, ulong itemId, IReadOnlyList<byte> stain, uint key = 0, ApplyFlag flags = ApplyFlag.Once)
+    public GlamourerApiEc Invoke(int objectIndex, ApiEquipSlot slot, ulong itemId, IReadOnlyList<byte> stain, uint key = 0,
+        ApplyFlag flags = ApplyFlag.Once)
         => (GlamourerApiEc)Invoke(objectIndex, (byte)slot, itemId, stain, key, (ulong)flags);
 
     /// <summary> Create a provider. </summary>
-    public static FuncProvider<int, byte, ulong, IReadOnlyList<byte>, uint, ulong, int> Provider(IDalamudPluginInterface pi, IGlamourerApiItems api)
+    public static FuncProvider<int, byte, ulong, IReadOnlyList<byte>, uint, ulong, int> Provider(IDalamudPluginInterface pi,
+        IGlamourerApiItems api)
         => new(pi, Label, (a, b, c, d, e, f) => (int)api.SetItem(a, (ApiEquipSlot)b, c, d, e, (ApplyFlag)f));
 }
 
@@ -29,10 +31,44 @@ public sealed class SetItemName(IDalamudPluginInterface pi)
     public const string Label = $"Glamourer.{nameof(SetItemName)}.V2";
 
     /// <inheritdoc cref="IGlamourerApiItems.SetItem"/>
-    public GlamourerApiEc Invoke(string objectName, ApiEquipSlot slot, ulong itemId, IReadOnlyList<byte> stain, uint key = 0, ApplyFlag flags = ApplyFlag.Once)
+    public GlamourerApiEc Invoke(string objectName, ApiEquipSlot slot, ulong itemId, IReadOnlyList<byte> stain, uint key = 0,
+        ApplyFlag flags = ApplyFlag.Once)
         => (GlamourerApiEc)Invoke(objectName, (byte)slot, itemId, stain, key, (ulong)flags);
 
     /// <summary> Create a provider. </summary>
-    public static FuncProvider<string, byte, ulong, IReadOnlyList<byte>, uint, ulong, int> Provider(IDalamudPluginInterface pi, IGlamourerApiItems api)
+    public static FuncProvider<string, byte, ulong, IReadOnlyList<byte>, uint, ulong, int> Provider(IDalamudPluginInterface pi,
+        IGlamourerApiItems api)
         => new(pi, Label, (a, b, c, d, e, f) => (int)api.SetItemName(a, (ApiEquipSlot)b, c, d, e, (ApplyFlag)f));
+}
+
+/// <inheritdoc cref="IGlamourerApiItems.SetBonusItem"/>
+public sealed class SetBonusItem(IDalamudPluginInterface pi)
+    : FuncSubscriber<int, byte, ulong, uint, ulong, int>(pi, Label)
+{
+    /// <summary> The label. </summary>
+    public const string Label = $"Glamourer.{nameof(SetBonusItem)}";
+
+    /// <inheritdoc cref="IGlamourerApiItems.SetBonusItem"/>
+    public GlamourerApiEc Invoke(int objectIndex, ApiBonusSlot slot, ulong itemId, uint key = 0, ApplyFlag flags = ApplyFlag.Once)
+        => (GlamourerApiEc)Invoke(objectIndex, (byte)slot, itemId, key, (ulong)flags);
+
+    /// <summary> Create a provider. </summary>
+    public static FuncProvider<int, byte, ulong, uint, ulong, int> Provider(IDalamudPluginInterface pi, IGlamourerApiItems api)
+        => new(pi, Label, (a, b, c, d, e) => (int)api.SetBonusItem(a, (ApiBonusSlot)b, c, d, (ApplyFlag)e));
+}
+
+/// <inheritdoc cref="IGlamourerApiItems.SetBonusItemName"/>
+public sealed class SetBonusItemName(IDalamudPluginInterface pi)
+    : FuncSubscriber<string, byte, ulong, uint, ulong, int>(pi, Label)
+{
+    /// <summary> The label. </summary>
+    public const string Label = $"Glamourer.{nameof(SetBonusItemName)}.V2";
+
+    /// <inheritdoc cref="IGlamourerApiItems.SetBonusItemName"/>
+    public GlamourerApiEc Invoke(string objectName, ApiBonusSlot slot, ulong itemId, uint key = 0, ApplyFlag flags = ApplyFlag.Once)
+        => (GlamourerApiEc)Invoke(objectName, (byte)slot, itemId, key, (ulong)flags);
+
+    /// <summary> Create a provider. </summary>
+    public static FuncProvider<string, byte, ulong, uint, ulong, int> Provider(IDalamudPluginInterface pi, IGlamourerApiItems api)
+        => new(pi, Label, (a, b, c, d, e) => (int)api.SetBonusItemName(a, (ApiBonusSlot)b, c, d, (ApplyFlag)e));
 }
